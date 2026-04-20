8bc2ea7a84fbee26ef65dd3846fc695d96618e61
Catelyn <catelynhansen07@gmail.com>
2026-04-20 14:32:26 +0200

    populated database

--- store/management/commands/seed_db.py
+++ store/management/commands/seed_db.py
@@ -7,10 +7,17 @@ class Command(BaseCommand):
     help = 'Populates the database with collections and products'
 
     def handle(self, *args, **options):
-        print('Populating the database...')
+        self.stdout.write("Populating the database...")
+
         current_dir = os.path.dirname(__file__)
         file_path = os.path.join(current_dir, 'seed.sql')
+
         sql = Path(file_path).read_text()
 
         with connection.cursor() as cursor:
-            cursor.execute(sql)
+            for statement in sql.split(";"):
+                stmt = statement.strip()
+                if stmt:
+                    cursor.execute(stmt)
+
+        self.stdout.write(self.style.SUCCESS("Database seeded successfully!"))
